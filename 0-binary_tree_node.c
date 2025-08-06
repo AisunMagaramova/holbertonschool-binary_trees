48c9492c1b15e6570f4d8d918229587b560bfbb1
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-08-06 12:01:08 +0000

    task0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,9 +8,9 @@
  * Return: Pointer to the new node, or NULL if memory allocation fails.
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
-
 {
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+
 	if (!new_node)
 		return NULL;
 
@@ -19,5 +19,5 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new_node->left = NULL;
 	new_node->right = NULL;
 
-	return new_node;
+	return (new_node);
 }
